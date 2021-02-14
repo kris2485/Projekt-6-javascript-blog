c42e8a295015433676fc2ee5fd813f874799a4e6
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-02-14 13:34:44 +0100

    Edit script.js - add const clickedElement & add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -6,25 +6,29 @@ document.getElementById('test-button').addEventListener('click', function(){
 });
 */
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
+	const clickedElement = this;
+  	console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [Done] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
   	activeLink.classList.remove('active');
    }
 
-  /* add class 'active' to the clicked link */
+  /* [Done] add class 'active' to the clicked link */
+
+  	clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
+
+  /* [Done] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll(' .posts .active');
 
   for (let activeArticle of activeArticles) {
   	activeArticle.classList.remove('active');
   }
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
