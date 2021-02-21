706dd5199e50e9d21f92260503d9ba371f348491
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-02-21 16:56:21 +0100

    Edit html file - removed content from .post-author & add data-author

--- js/script.js
+++ js/script.js
@@ -49,7 +49,8 @@ const titleClickHandler = function (event) {
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
-  optArticleTagsSelector = '.post-tags .list';
+  optArticleTagsSelector = '.post-tags .list',
+  optArticleAuthorSelector = '.post .post-author';
 
 function generateTitleLinks(customSelector = '') {
   /* [Done] remove contents of titleList */
